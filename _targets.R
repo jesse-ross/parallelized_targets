4b14b9aeaa85c959221d994dc3bbb57ebfbed4aa
Jesse Ross <jross@usgs.gov>
2021-12-20 15:08:15 -0800

    fix penguins example without parallelism

--- _targets.R
+++ _targets.R
@@ -1,15 +1,19 @@
 library(dplyr)
-library(targets)                                                                                                        tar_option_set(packages = c("palmerpenguins"))
+library(palmerpenguins)
+library(tarchetypes)
+library(targets)
+tar_option_set(packages = c("palmerpenguins"))
 #options(clustermq.scheduler = "multiprocess")
 spp = as.character(unique(penguins$species))
 list(
-  tar_target(penguins_cleaned = filter(penguins,
-				       ! is.na(body_mass_g),
-				       ! is.na(sex))),
+  tar_target(penguins_cleaned,
+             filter(penguins,
+                    ! is.na(body_mass_g),
+                    ! is.na(sex))),
   tar_map(
-    values = tibble(spp),
+    values = tibble(spp = spp),
     names = spp,
-    tar_target(sp, filter(penguins, species == sp)),
+    tar_target(sp, filter(penguins_cleaned, species == spp)),
     tar_target(mean_body_mass, mean(sp$body_mass_g / 1000)),
     tar_target(male_female_ratio, sum(sp$sex == "male") / sum(sp$sex == "female"))
     )
